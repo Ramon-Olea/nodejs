bbc8e716303d17061d65160abe12008ff5ab2464
fdg <olearamon3@gmail.com>
2024-06-13 14:30:07 -0600

    aa

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Motor de plantilla
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+app.get("/", (req, res) => {
+  res.render('index',{
+    titulo : "titulo",
+    descripcion: "descripcion"
+  });
+});
+app.get("/servicios", (req, res) => {
+  res.render('servicios',{titulo : "servicios"});
+});
+
+app.use((req, res,next) => {
+  res.render('404');
+});
+app.use(express.static(__dirname +'/public'));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ letras.js
@@ -0,0 +1,7 @@
+const letras = ["a", "b", "c", "d", "e", "f"];
+const dinero = ['1','2','3','4','5','6','7','8','9'];
+module.exports= {
+  letras:letras,  
+  dinero:dinero
+
+};   //exportaos la ruta 
